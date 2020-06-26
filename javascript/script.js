c040b62d017b153fb252c0bdc2ff7a79715e7428
lindyem <lindy.merwin@gmail.com>
2020-06-26 12:47:45 -0700

    ugh

--- javascript/script.js
+++ javascript/script.js
@@ -1,59 +1,58 @@
 //FUNCTION COUNTRY CHANGE
 function handleCountryChange(lat, lng) {
-    $.ajax({
-      url: `https://api.opencagedata.com/geocode/v1/json?q=${lat}+${lng}&key=f3d378ee02e1426d89edc3bae843d163`,
-      method: "GET",
-    }).then(function (response) {
-      var countryCode =
-        response.results[0].components["ISO_3166-1_alpha-2"];
-  
-      renderTopCharts(countryCode);
-    });
-  }
-
-//YOUTUBE TOP CHARTS FUNCTION
-function renderTopCharts(countryCode) {
+  $.ajax({
+    url: `https://api.opencagedata.com/geocode/v1/json?q=${lat}+${lng}&key=f3d378ee02e1426d89edc3bae843d163`,
+    method: "GET",
+  }).then(function (response) {
+    var countryCode = response.results[0].components["ISO_3166-1_alpha-2"];
+    renderTopCharts(countryCode);
+  });
+  //YOUTUBE TOP CHARTS FUNCTION
+  function renderTopCharts(countryCode) {
     $("iframe").css("display", "block");
     $("#map").css("height", "50%");
     $.ajax({
-      url: `https://www.googleapis.com/youtube/v3/videos/?part=snippet&chart=mostPopular&regionCode=${countryCode}&key=AIzaSyCRwKFOIGwE09uqNuhJJUoOg5Oxa9jbB1M`,
+      url: `https://www.googleapis.com/youtube/v3/videos/?part=snippet&chart=mostPopular&regionCode=${countryCode}&key=AIzaSyCCEa_pI9HJijLBIAoVxtRplDO1DM0MrQk`,
       method: "GET",
     }).then(function (response) {
       $("#videoList").empty();
       for (var i = 0; i < response.items.length; i++) {
         var newDiv = $("<div>");
         newDiv.html(`
-          <div class='videoListItem' videoId=${response.items[i].id}> 
-            <div>${response.items[i].snippet.title}</div>
-            <img src=${response.items[i].snippet.thumbnails.default.url} />
-          </div>
-        `);
-        $("iframe").attr("src", `https://www.youtube.com/embed/${videoId}`);
-            var descripDiv = $("<div>");
-            descripDiv.text(vidDescription);
-            console.log(vidDescription);
-            console.log(descripDiv);
-            $("#iframeId").append(descripDiv)
+        <div class=videoListItem videoId=${response.items[i].id} videoDescription=${response.items[i].snippet.description}>
+          <div>${response.items[i].snippet.title}</div>
+          <img src=${response.items[i].snippet.thumbnails.default.url} />
+        </div>
+      `);
         $("#videoList").append(newDiv);
       }
+      $(".videoListItem").click(function () {
+        var selectedVideoId = $(this).attr("videoId");
+        var selectedVideoDescription = $(this).attr("videoDescription");
+        $("iframe").attr(
+          "src",
+          `https://www.youtube.com/embed/${selectedVideoId}`
+        );
+        $("#videoDescription").text(selectedVideoDescription);
+      });
     });
   }
-
+}
 //INIT MAP FUNCTION
 function initMap() {
-    //Sets initial coord
-    var myLatlng = { lat: 0, lng: 0 };
-    var map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 3,
-      center: myLatlng,
-    });
-    var marker = new google.maps.Marker({
-      position: myLatlng,
-      map: map,
-    });
-    map.addListener("click", function (event) {
-      // event.latLng = {lat: 0, lng: 0}
-      marker.setPosition(event.latLng);
-      handleCountryChange(event.latLng.lat(), event.latLng.lng());
-    });
-  }
+  //Sets initial coord
+  var myLatlng = { lat: 0, lng: 0 };
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 3,
+    center: myLatlng,
+  });
+  var marker = new google.maps.Marker({
+    position: myLatlng,
+    map: map,
+  });
+  map.addListener("click", function (event) {
+    // event.latLng = {lat: 0, lng: 0}
+    marker.setPosition(event.latLng);
+    handleCountryChange(event.latLng.lat(), event.latLng.lng());
+  });
+}
